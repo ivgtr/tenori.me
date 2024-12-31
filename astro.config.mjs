334e9815f3baa00d5c1b69ef77c7499ba459b438
ivgtr <ivgtr.me@gmail.com>
2025-01-01 00:44:17 +0900

    Vercelアダプターを削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,9 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel";
-
 // https://astro.build/config
 export default defineConfig({
 	prefetch: true,
 	site: "https://example.com",
 	integrations: [mdx(), sitemap(), tailwind()],
-	adapter: vercel(),
 });
