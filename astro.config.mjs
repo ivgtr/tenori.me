75ebf04057858d8d64182a45c6ff844863d00fc1
ivgtr <ivgtr.me@gmail.com>
2024-12-31 23:39:40 +0900

    Prettier設定ファイルを追加し、コードフォーマットを統一。新しいコンポーネントを追加し、サイトタイトルと説明を更新。記事ページを作成し、不要なファイルを削除。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  adapter: vercel(),
+	prefetch: true,
+	site: "https://example.com",
+	integrations: [mdx(), sitemap(), tailwind()],
+	adapter: vercel(),
 });
