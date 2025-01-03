3ef1fc857469f388d58634483aa576e75b746b5b
ivgtr <ivgtr.me@gmail.com>
2025-01-04 05:01:58 +0900

    動的にデータ取得したいページをSSRする

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    prefetch: true,
-    site: "https://example.com",
-    integrations: [mdx(), sitemap(), tailwind(), react()],
+	prefetch: true,
+	site: "https://example.com",
+	integrations: [mdx(), sitemap(), tailwind(), react()],
+	adapter: cloudflare(),
 });
